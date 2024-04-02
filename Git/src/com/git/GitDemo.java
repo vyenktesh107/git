fc840ee8c69f0b1ca4d13af87d9cee4a753ddf15
vyenktesh107 <165635414+vyenktesh107@users.noreply.github.com>
2024-04-02 12:35:31 +0530

    Update GitDemo.java
    added b = 60

--- Git/src/com/git/GitDemo.java
+++ Git/src/com/git/GitDemo.java
@@ -3,7 +3,7 @@ package com.git;
 public class GitDemo {
 	
 	int a = 10;
-	int b = 40;
+	int b = 60;
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		System.out.println("Hello Git");

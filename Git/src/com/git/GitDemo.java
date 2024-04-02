440dd7f0af7bb77c85098fe97a138419ae1122e8
Vyenktesh Patil <vyenkteshpatil02@gmail.com>
2024-04-02 12:23:09 +0530

    changed b to 40

--- Git/src/com/git/GitDemo.java
+++ Git/src/com/git/GitDemo.java
@@ -3,7 +3,7 @@ package com.git;
 public class GitDemo {
 	
 	int a = 10;
-	int b = 30;
+	int b = 40;
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		System.out.println("Hello Git");
